dbadaa6531a0f7b31cd025adf197ad415989c6f4
eel <unagiman@icloud.com>
2022-10-21 09:48:42 -0600

    Pkmn

--- app.js
+++ app.js
@@ -1,69 +1,83 @@
-class Person {
-    //creates instance of Person with properties name, age, and numBooksRead
-    constructor(name, age, numBooksRead) {
+class Pokemon {
+    //creates instance of Pokemon with properties name, lvl, hp, and moves
+    constructor(name, lvl, hp, moves) {
         this.name = name;
-        this.age = age;
-        this.numBooksRead = numBooksRead;
+        this.lvl = lvl;
+        this.hp = hp;
+        this.moves = moves;
     }
 
-    //increments numBooksRead by 1
-    readNewBook() {
-        this.numBooksRead++;
+    //increments lvl by 1 and hp +15
+    rareCandy() {
+        this.lvl++;
+        this.hp += 15;
+    }
+
+    //decrements hp by 20
+    attack() {
+        this.hp -= 20;
+    }
+
+    //potion hp by 10
+    potion() {
+        this.hp += 10;
+    }
+
+}
+
+class FireType extends Pokemon {
+    constructor(name, lvl, hp, moves) {
+        super(name, lvl, hp, moves);
+        this.moves = moves;
     }
 }
 
-class Electrician extends Person {
-    //creates instance of Electrician with Person properites plus certifications, an array of strings
-    constructor(name, age, numBooksRead, certifications) {
-        super(name, age, numBooksRead);
-        this.certifications = certifications;
-    }
-}
-
-class Teenager extends Person {
-    //creates instance of Teenager with Person properties plus isHungry, a boolean value
-    constructor(name, age, numBooksRead, isHungry = true) {
-        super(name, age, numBooksRead);
-        this.isHungry = isHungry;
+class GhostType extends Pokemon {
+    constructor(name, lvl, hp, moves, effectedBy = true) {
+        super(name, lvl, hp, moves);
+        this.effectedBy = effectedBy;
     }
 
-    //feeds the teenager.  If they are hungry, changes isHungry to false, if they are not hungry, prints message to console.
-    eat() {
-        if(this.isHungry) {
-            this.isHungry = false;
+    effect() {
+        if(this.effectedBy) {
+            this.effectedBy = true;
+            console.log('It was super effective!')
         } else {
-            console.log('Oh no I think I ate too much')
+            console.log('Has no effect.')
         }
     }
 }
 
-//below we will test our classes by instantiating them, calling each method, and verifying output
 
-//create instances of each class
-let person = new Person('Euthyphro', 35, 0);
-let electrician = new Electrician('Zeus', 28, 3, ['Lightning', 'Polymorphism']);
-let teen = new Teenager('Persius', 16, 5, true);
+let fireType= new FireType('Growlithe', 12, 35, ['Fireball', 'Bite']);
+console.log('A wild Growlithe appeared!!!');
+console.log(fireType);
 
-//log each instance
-console.log(person);
-console.log(electrician);
-console.log(teen);
+let ghostType = new GhostType('Ghastly', 48, 72, ['Curse', 'Lick'], false);
+console.log('Go Ghastly!!!');
+console.log(ghostType);
 
-//call the method from the parent class to ensure it works as expected
-person.readNewBook();
-electrician.readNewBook();
-teen.readNewBook();
 
-//log the objects after calling .readNewBook() to verify it worked as intended
-console.log(person);
-console.log(electrician);
-console.log(teen);
 
-//call .eat()
-teen.eat();
+fireType.attack();
+console.log('Ghastly used Lick! -20 HP')
+console.log(fireType);
 
-//check that .eat() when isHungry == true works as expected
-console.log(teen);
+console.log('Growlithe used Bite!');
+ghostType.effect();
+console.log(ghostType);
 
-//check that .eat() when isHungry == false works as expected
-teen.eat();
+console.log('Trainer gave Ghastly a Potion! +10 HP');
+ghostType.potion();
+console.log(ghostType);
+
+console.log('Growlithe used Fireball!');
+ghostType.attack();
+console.log(ghostType);
+
+fireType.attack();
+console.log('Ghastly used Lick! -20 HP')
+console.log(fireType);
+console.log('You Killed Growlith D:')
+
+
